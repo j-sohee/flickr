647803fdf4577e5aaabfc655085fc7287f9d15b2
j-sohee <sohee.jin@gmail.com>
2021-10-11 17:38:55 +0900

    gallery작업

---
+++ js/myFlickr.js
@@ -0,0 +1,58 @@
+/*
+// key : c28561608d1c0e9f7db60ecfda79bf27
+//원하는 이미지 출력
+https://www.flickr.com/services/rest/?method=flickr.photos.search
+
+//사진경로
+https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
+
+*/
+
+$.ajax({
+    url : "https://www.flickr.com/services/rest/?method=flickr.photos.search",
+    dataType : "json",
+    data : {
+        api_key : "c28561608d1c0e9f7db60ecfda79bf27",
+        per_page : 16,
+        format: "json",
+        nojsoncallback:1,
+        privacy_filter:5,
+        tags : "Moonlit"
+    }
+})
+
+.success(function(data){
+    console.log(data.photos.photo);
+    let items = data.photos.photo;
+
+    $(".myImg").append("<ul>");
+    $(items).each(function(index,data){
+
+        let text = data.title; 
+        if(!data.title){
+            text = "No description in this photo";
+        }
+
+        $(".myImg ul")
+        .append(
+            $("<li>")
+            .append(
+                $("<a>").attr({
+                    href : "https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_b.jpg"
+                })
+                    .append(
+                        $("<img>").attr({ src : "https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_b.jpg" })
+                    )
+            )
+            .append(
+                $("<span>").text("Lorem, ipsum dolor")
+            )
+            .append(
+                $("<h2>").text(text)
+            )
+        )
+    })
+})
+.error(function(err){
+    console.err("데이터를 호출하는데 실패했습니다");
+})
